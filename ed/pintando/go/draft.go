02ebc9074fe9eae21a36cfa04af77251f9a86791
Artur Sousa <artursousa092@gmail.com>
2026-03-16 15:41:40 -0300

    questão pintando

---
+++ ed/pintando/go/draft.go
@@ -0,0 +1,15 @@
+package main
+import ("fmt"
+        "math")
+
+func main() {
+    
+    var a, b, c float64
+    fmt.Scan(&a, &b, &c)
+    p := (a + b + c)/2
+
+    result := math.Sqrt( p * ( p - a ) * ( p - b ) * (p - c ))
+
+    fmt.Printf("%.2f", result)
+
+}
