1615d7de0000c3dff6c529239e6b2bed4febbe11
Artur Sousa <artursousa092@gmail.com>
2026-05-11 10:23:03 -0300

    questao resto

--- ed/resto/src/go/draft.go
+++ ed/resto/src/go/draft.go
@@ -1,5 +1,25 @@
 package main
+
 import "fmt"
+
+func solve(n int) {
+
+	if n == 0 {
+		return
+	}
+	div := n / 2
+	resto := n % 2
+	solve(div)
+
+	fmt.Printf("%d %d\n", div, resto)
+}
 func main() {
-    fmt.Println("Hello, World!")
+
+	var n int
+	_, err := fmt.Scan(&n)
+	if err != nil {
+		return
+	}
+	solve(n)
+
 }
