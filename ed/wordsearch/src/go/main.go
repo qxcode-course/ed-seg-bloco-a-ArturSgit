8eace76bd7d1e27bfbbaebfa4e51a61e535640a4
Artur de Sousa Lima <artursousa092@gmail.com>
2026-05-31 18:17:54 -0300

    questao wordsearch

--- ed/wordsearch/src/go/main.go
+++ ed/wordsearch/src/go/main.go
@@ -6,10 +6,53 @@ import (
 	"os"
 )
 
-// Não mude a assinatura desta função, ela é a função chamada pelo LeetCode
 func exist(grid [][]byte, word string) bool {
-	_, _ = grid, word
+	if len(grid) == 0 || len(grid[0]) == 0 {
+		return false
+	}
+
+	rows := len(grid)
+	cols := len(grid[0])
+
+	if len(word) > rows*cols {
+		return false
+	}
+
+	for i := 0; i < rows; i++ {
+
+		for j := 0; j < cols; j++ {
+
+			if grid[i][j] == word[0] {
+
+				if dfs(grid, word, i, j, 0) {
+					return true
+				}
+			}
+		}
+	}
 	return false
+}
+
+func dfs(grid [][]byte, word string, i, j, index int) bool {
+
+	if index == len(word) {
+		return true
+	}
+
+	if i < 0 || i >= len(grid) || j < 0 || j >= len(grid[0]) || grid[i][j] != word[index] {
+		return false
+	}
+
+	temp := grid[i][j]
+	grid[i][j] = '#'
+
+	found := dfs(grid, word, i+1, j, index+1) ||
+		dfs(grid, word, i-1, j, index+1) ||
+		dfs(grid, word, i, j+1, index+1) ||
+		dfs(grid, word, i, j-1, index+1)
+
+	grid[i][j] = temp
+	return found
 }
 
 func main() {
