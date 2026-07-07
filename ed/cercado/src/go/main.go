703b65577b506fee7c88f6695c842974a4cdcb3d
Artur de Sousa Lima <artursousa092@gmail.com>
2026-07-07 12:53:44 -0300

    questao cercado

--- ed/cercado/src/go/main.go
+++ ed/cercado/src/go/main.go
@@ -8,7 +8,52 @@ import (
 
 // NÃO ALTERE A ASSINATURA DA FUNÇÃO solve
 func solve(board [][]byte) {
-	_ = board
+	if len(board) == 0 || len(board[0]) == 0 {
+		return
+	}
+	nrows := len(board)
+	ncols := len(board[0])
+
+	var dfs func(r, c int)
+	dfs = func(r, c int) {
+		if r < 0 || r >= nrows || c < 0 || c >= ncols || board[r][c] != 'O' {
+			return
+		}
+		board[r][c] = 'T' // Marca como temporário para evitar ciclos
+		dfs(r-1, c)       // Cima
+		dfs(r+1, c)       // Baixo
+		dfs(r, c-1)       // Esquerda
+		dfs(r, c+1)       // Direita
+	}
+
+	for i := 0; i < nrows; i++ {
+		if board[i][0] == 'O' {
+
+			dfs(i, 0)
+		}
+		if board[i][ncols-1] == 'O' {
+			dfs(i, ncols-1)
+		}
+	}
+
+	for j := 0; j < ncols; j++ {
+		if board[0][j] == 'O' {
+			dfs(0, j)
+		}
+		if board[nrows-1][j] == 'O' {
+			dfs(nrows-1, j)
+		}
+	}
+
+	for i := 0; i < nrows; i++ {
+		for j := 0; j < ncols; j++ {
+			if board[i][j] == 'O' {
+				board[i][j] = 'X'
+			} else if board[i][j] == 'T' {
+				board[i][j] = 'O'
+			}
+		}
+	}
 }
 
 // NÃO ALTERE A MAIN
